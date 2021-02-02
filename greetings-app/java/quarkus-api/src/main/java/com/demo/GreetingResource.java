febeb8d33cbc992b16bc48b8c52ac01ba3b7e7de
Naveen Kendyala <subscribeken@gmail.com>
2021-02-02 18:04:59 -0500

    Trigger Demo

--- greetings-app/java/quarkus-api/src/main/java/com/demo/GreetingResource.java
+++ greetings-app/java/quarkus-api/src/main/java/com/demo/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Tekton Rocks !!";
+        return "Tekton Trigger Demo !!";
     }
 }
